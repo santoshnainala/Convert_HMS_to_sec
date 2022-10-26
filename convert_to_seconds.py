11089aebd7009ea514522d4dd38627b7d019170a
santoshnainala <51361959+santoshnainala@users.noreply.github.com>
2022-10-26 22:23:30 +0530

    Add files via upload

---
+++ convert_to_seconds.py
@@ -0,0 +1,5 @@
+#This code is used to convert given hours, minutes and seconds into total number of seconds
+a=int(input("Enter hours : "))
+b=int(input("Enter minutes : "))
+c=int(input("Enter seconds : "))
+print("The number of seconds in",a,"hours",b,"minutes and",c,"seconds is :",(a*60*60)+(b*60)+c,"seconds")
